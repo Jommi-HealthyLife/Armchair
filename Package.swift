fa865826ca67363a9394734783181ac4b6373c8c
Mariusz Graczkowski <mariusz.graczkowski@digitalforms.pl>
2021-11-01 10:54:04 +0100

    Update PackageDescription

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "Armchair"
+    name: "Armchair",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "Armchair",
+            targets: ["Armchair"]),
+    ],
+    targets: [
+        .target(
+            name: "Armchair",
+            path: "Source"),
+    ]
 )
